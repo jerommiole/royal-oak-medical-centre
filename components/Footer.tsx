983277f75fd6f84d7b5bca006458c29faede294d
Jerom Miole <miolejerom@gmail.com>
2023-10-15 23:23:21 +1300

    displaying navbar with logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
-import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from '@/constants'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
   return (
@@ -9,16 +9,21 @@ const Footer = () => {
       <div className="padding-container max-container flex w-full flex-col gap-14">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
           <Link href="/" className="mb-10">
-            <Image src="hilink-logo.svg" alt="logo" width={74} height={29}/>
+            <Image
+              src="/romc-vertical-logo.png"
+              alt="logo"
+              width={74}
+              height={29}
+            />
           </Link>
 
-          <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
+          <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
             {FOOTER_LINKS.map((columns) => (
-              <FooterColumn title={columns.title}>
+              <FooterColumn title={columns.title} key={columns.title}>
                 <ul className="regular-14 flex flex-col gap-4 text-gray-30">
                   {columns.links.map((link) => (
-                    <Link href="/" key={link}>
-                      {link}
+                    <Link href={link.url} key={link.url}>
+                      {link.text}
                     </Link>
                   ))}
                 </ul>
@@ -29,13 +34,11 @@ const Footer = () => {
               <FooterColumn title={FOOTER_CONTACT_INFO.title}>
                 {FOOTER_CONTACT_INFO.links.map((link) => (
                   <Link
-                    href="/"
+                    href={link.value}
                     key={link.label}
                     className="flex gap-4 md:flex-col lg:flex-row"
                   >
-                    <p className="whitespace-nowrap">
-                      {link.label}:
-                    </p>
+                    <p className="whitespace-nowrap">{link.label}:</p>
                     <p className="medium-14 whitespace-nowrap text-blue-70">
                       {link.value}
                     </p>
@@ -43,32 +46,23 @@ const Footer = () => {
                 ))}
               </FooterColumn>
             </div>
-
-            <div className="flex flex-col gap-5">
-              <FooterColumn title={SOCIALS.title}>
-                <ul className="regular-14 flex gap-4 text-gray-30">
-                  {SOCIALS.links.map((link) => (
-                    <Link href="/" key={link}>
-                      <Image src={link} alt="logo" width={24} height={24} />
-                    </Link>
-                  ))}
-                </ul>
-              </FooterColumn>
-            </div>
           </div>
         </div>
 
         <div className="border bg-gray-20" />
-        <p className="regular-14 w-full text-center text-gray-30">2023 Hilink | All rights reserved</p>
+        <p className="regular-14 w-full text-center text-gray-30">
+          2023 Royal Oack Medical Centre | All rights reserved <br />
+          Designed by: jrom
+        </p>
       </div>
     </footer>
-  )
-}
+  );
+};
 
 type FooterColumnProps = {
   title: string;
   children: React.ReactNode;
-}
+};
 
 const FooterColumn = ({ title, children }: FooterColumnProps) => {
   return (
@@ -76,7 +70,7 @@ const FooterColumn = ({ title, children }: FooterColumnProps) => {
       <h4 className="bold-18 whitespace-nowrap">{title}</h4>
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
